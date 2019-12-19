69fa8fc7848c88227d5c4f049ac695caac6e433e
Adarsh Pastakia <adarshpastakia@outlook.com>
2019-12-19 17:16:36 +0400

    eslint update

--- src/__stories__/Welcome.stories.tsx
+++ src/__stories__/Welcome.stories.tsx
@@ -2,17 +2,17 @@ import { storiesOf } from "@storybook/react";
 import React from "react";
 
 storiesOf("Welcome", module)
-    .addParameters({
-        info: {
-            disable: true
-        },
-        options: {
-            showPanel: false
-        }
-    })
-    .add("Introduction", () => (
-        <div>
-            <h1>ReactJs Library Template</h1>
-            <p>Storybook for ReactJs library</p>
-        </div>
-    ));
+  .addParameters({
+    info: {
+      disable: true
+    },
+    options: {
+      showPanel: false
+    }
+  })
+  .add("Introduction", () => (
+    <div>
+      <h1>ReactJs Library Template</h1>
+      <p>Storybook for ReactJs library</p>
+    </div>
+  ));
