c6b9b4c8badc8f4699eb963415c879af01472c47
Adarsh Pastakia <adarshpastakia@outlook.com>
2019-12-19 14:29:53 +0400

    Update global.d.ts

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -1,11 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare type AnyObject = any;
 declare type KeyValue<T = AnyObject> = { [key: string]: T };
 
 declare module "*.png" {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const content: any;
   export default content;
 }
 declare module "*.svg" {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const content: any;
   export default content;
 }
